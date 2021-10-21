64b27372396304db9f9b6e1597b8f66b57aa9f2e
ubar667 <ayoub.rhim@eleves.enpc.fr>
2021-10-21 10:50:51 +0200

    Add h-convex transformation function

--- layers.py
+++ layers.py
@@ -338,6 +338,20 @@ def h_maxima_transform(X, steps=None):
     HMAX = geodesic_dilation([Marker, Mask], steps=steps)
     return  HMAX
 
+@tf.function
+def h_convex_transform(X, steps=None):
+    """
+    h-maxima transform of image X[1] with h=X[0]
+    :X tensor: X[0] is h and X[1] is the Image
+    :param steps: number of steps (by default NUM_ITER_REC)
+    :Example:
+    >>>Lambda(h_maxima_transform, name="h-maxima")([h,Image])
+    """
+    h = X[0]
+    Mask = X[1]
+    Marker = Mask - h
+    HCONVEX = Mask - geodesic_dilation([Marker, Mask], steps=steps)
+    return  HCONVEX
 
 """
 ====================
